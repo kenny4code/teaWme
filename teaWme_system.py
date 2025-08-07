19dddecc8a23deb2c41979a5a32fa43cbfec9f1a
Kenny <nguyentankhang.ken@gmail.com>
2025-08-07 16:52:33 +0700

    System Python Code

---
+++ teaWme_system.py
@@ -0,0 +1,19 @@
+import json
+tea_input = 'D:\\kennguyen\\CODING\\teaWme\\database_teaWme_input.json'
+tea_output = 'D:\\kennguyen\\CODING\\teaWme\\database_teaWme_output.json'
+
+def add_order():
+    print("WELCOME TO TEAWME - READY TO ORDER YOUR DRINK?")
+    print([
+    {'id': 1, 'name': 'Trà sữa truyền thống'},
+    {'id': 2, 'name': 'Trà sữa trân châu đường đen'},
+    {'id': 3, 'name': 'Trà sữa Thái xanh'},
+    {'id': 4, 'name': 'Trà sữa socola'},
+    {'id': 5, 'name': 'Trà sữa matcha'},
+    {'id': 6, 'name': 'Trà sữa khoai môn'},
+    {'id': 7, 'name': 'Trà sữa caramel'},
+    {'id': 8, 'name': 'Trà sữa hokkaido'},
+    {'id': 9, 'name': 'Trà sữa bạc hà'},
+    {'id': 10, 'name': 'Trà sữa dâu'}
+])
+print(add_order)
